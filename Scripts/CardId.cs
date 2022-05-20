69408529fd7b092b3d92da7c3d762ec45c27a2ef
Benjamin Delvert <benjamin.delvert@epitech.eu>
2022-05-20 21:43:43 +0200

    FIX : sprite to material to apply it to 3d model

--- Scripts/CardId.cs
+++ Scripts/CardId.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName="Card_Id", menuName="Devcompagnie/Jam3/CardId", order = 1)]
 public class CardId : ScriptableObject
 {
-    public Sprite Image;
+    public Material Image;
     public FamilyId family;
     public string Name;
     public Color color;
